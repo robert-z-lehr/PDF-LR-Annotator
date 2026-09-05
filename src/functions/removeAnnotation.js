b38cba14717fa190acb7dc25517ebafb4add13d5
Robert Lehr <robert.z.lehr@gmail.com>
2026-09-05 15:40:32 -0500

    Update legend counts when highlights are removed

--- src/functions/removeAnnotation.js
+++ src/functions/removeAnnotation.js
@@ -1,5 +1,6 @@
 import {state} from '../state.js';
 import {rebuildCategoryIndex} from './rebuildCategoryIndex.js';
+import {updateLegendCounts} from './updateLegendCounts.js';
 import {updateStatus} from './updateStatus.js';
 import {closeCommentBox} from './closeCommentBox.js';
 export function removeAnnotation(id){
@@ -7,5 +8,6 @@ export function removeAnnotation(id){
   state.annotations=state.annotations.filter(annotation=>annotation.id!==id);
   document.querySelectorAll(`.highlight[data-id="${CSS.escape(id)}"]`).forEach(el=>el.remove());
   rebuildCategoryIndex();
+  updateLegendCounts();
   updateStatus();
 }
