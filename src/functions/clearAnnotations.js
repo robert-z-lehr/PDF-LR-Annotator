4c4486def63766dd5404a2bf75e0655583ab2410
Robert Lehr <robert.z.lehr@gmail.com>
2026-09-05 15:40:37 -0500

    Reset legend counts when highlights are cleared

--- src/functions/clearAnnotations.js
+++ src/functions/clearAnnotations.js
@@ -1,10 +1,12 @@
 import {state} from '../state.js';
 import {rebuildCategoryIndex} from './rebuildCategoryIndex.js';
+import {updateLegendCounts} from './updateLegendCounts.js';
 import {updateStatus} from './updateStatus.js';
 export function clearAnnotations(){
   state.annotations=[];
   state.navCurrentIdByCategory.clear();
   document.querySelectorAll('.highlight').forEach(el=>el.remove());
   rebuildCategoryIndex();
+  updateLegendCounts();
   updateStatus();
 }
