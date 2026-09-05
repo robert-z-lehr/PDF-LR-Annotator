59dadea7d72eb779b3a525430a49f221fb31755b
Robert Lehr <robert.z.lehr@gmail.com>
2026-09-05 15:17:03 -0500

    Add PDF zoom state

--- src/state.js
+++ src/state.js
@@ -1,7 +1,11 @@
 export const state={
   pdf:null,
   fileName:null,
+  defaultScale:1.35,
   scale:1.35,
+  minScale:0.75,
+  maxScale:3,
+  zoomStep:0.15,
   annotations:[],
   selectedCategory:0,
   categoryIndex:new Map(),
