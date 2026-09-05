7e105f649ca0c3adc399027f8554be672dc65d5d
Robert Lehr <robert.z.lehr@gmail.com>
2026-09-05 15:04:31 -0500

    Add shared application state

---
+++ src/state.js
@@ -0,0 +1,11 @@
+export const state={
+  pdf:null,
+  fileName:null,
+  scale:1.35,
+  annotations:[],
+  selectedCategory:0,
+  categoryIndex:new Map(),
+  navCurrentIdByCategory:new Map(),
+  renderToken:0,
+  clickStart:null
+};
