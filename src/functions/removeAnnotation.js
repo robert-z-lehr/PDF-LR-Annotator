10b1f7346c4c1391a17865d6b8077d24ecafb160
Robert Lehr <robert.z.lehr@gmail.com>
2026-09-05 15:45:01 -0500

    Remove only deleted highlight's comment panel

--- src/functions/removeAnnotation.js
+++ src/functions/removeAnnotation.js
@@ -4,7 +4,7 @@ import {updateLegendCounts} from './updateLegendCounts.js';
 import {updateStatus} from './updateStatus.js';
 import {closeCommentBox} from './closeCommentBox.js';
 export function removeAnnotation(id){
-  if(state.openCommentId===id)closeCommentBox();
+  if(state.openCommentIds.has(id))closeCommentBox(id);
   state.annotations=state.annotations.filter(annotation=>annotation.id!==id);
   document.querySelectorAll(`.highlight[data-id="${CSS.escape(id)}"]`).forEach(el=>el.remove());
   rebuildCategoryIndex();
