7efbb5e2915547caee512a04f562b32b7ffbe449
Robert Lehr <robert.z.lehr@gmail.com>
2026-09-05 15:38:01 -0500

    Close comment box when deleting highlight

--- src/functions/removeAnnotation.js
+++ src/functions/removeAnnotation.js
@@ -1,7 +1,9 @@
 import {state} from '../state.js';
 import {rebuildCategoryIndex} from './rebuildCategoryIndex.js';
 import {updateStatus} from './updateStatus.js';
+import {closeCommentBox} from './closeCommentBox.js';
 export function removeAnnotation(id){
+  if(state.openCommentId===id)closeCommentBox();
   state.annotations=state.annotations.filter(annotation=>annotation.id!==id);
   document.querySelectorAll(`.highlight[data-id="${CSS.escape(id)}"]`).forEach(el=>el.remove());
   rebuildCategoryIndex();
